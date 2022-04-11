b8d43dda4807edf531a06c19e0db47dbfe9579bc
Kristan Frost <kristan.frost@gmail.com>
2022-04-11 14:42:05 -0500

    Changed overloaded method example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,9 +13,5 @@ public class Main {
         System.out.println(personTwo.getFullName());
         System.out.println(personTwo.getInitials());
 
-        // overloaded .roll() method
-//        System.out.println(Dice.roll()); // a number between 1 & 6
-//        System.out.println(Dice.roll(100)); // a number between 1 & 100
-//        System.out.println(Dice.roll(5, 15)); // a number between 5 & 15
     }
 }
